6520fc91eb4cf0f27358618cd94604f15526f999
MRDANANJAYA <#5'kfIm$s)#EjqxU>
2023-03-23 13:30:18 +0530

    App Creation

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'Dashboard/bottom_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TECHNICAL ASSESSMENT',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+
+      home: const BottomBar(),
+    );
+  }
+}
+
+
