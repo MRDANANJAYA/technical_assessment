cc8d483bd8d5cafbc26851800f4b8fc48cd97c5e
MRDANANJAYA <#5'kfIm$s)#EjqxU>
2023-03-24 15:21:23 +0530

    missing Internet permission added to manifest

--- lib/View/Tabs/businessTab.dart
+++ lib/View/Tabs/businessTab.dart
@@ -67,7 +67,7 @@ class _BusinessTabState extends State<BusinessTab> {
           isLoading = false;
         });
       }
-    });;
+    });
   }
 
 
--- lib/View/Widget/error_widget.dart
+++ lib/View/Widget/error_widget.dart
@@ -3,11 +3,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 onFailure(error,BuildContext context,onPressed){
-  if (kDebugMode) {
+
     ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: const Text('Unable to connect to server. Please try again later'),
       action: SnackBarAction(label: "Retry", onPressed: onPressed),
       behavior: SnackBarBehavior.floating,));
    // print("Some error ====> $error");
-  }
+
 }
